0cba63bfaf42454f5ef32f62a35294e7347ff965
Ajith K Sabu <ajith@strebentechnik.com>
2022-05-19 10:44:20 +0530

    removed mapper and POJO class

--- src/main/java/com/strebentechnik/logistix/hr/employee/EmployeeResource.java
+++ src/main/java/com/strebentechnik/logistix/hr/employee/EmployeeResource.java
@@ -1,14 +1,10 @@
 package com.strebentechnik.logistix.hr.employee;
 
-import java.util.Objects;
-
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,9 +17,6 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 @Path("/employees")
 @Tag(name = "Employees")
 public class EmployeeResource {
-
-    @Inject
-    EmployeeTransformer employeeTransformer;
 
     // Create Employee
     @POST
@@ -48,52 +41,7 @@ public class EmployeeResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getEmployeeById(@PathParam("id") Long id) {
         EmployeeEntity employeeEntity = EmployeeEntity.findById(id);
-        return Response.ok(employeeTransformer.toResource(employeeEntity)).build();
-    }
-
-    // Update employee
-    @Path("/{id}")
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    public Response updateEmployee(@PathParam("id") Long id, Employee employee) {
-        EmployeeEntity employeeEntity = EmployeeEntity.findById(id);
-
-        if (Objects.nonNull(employee.getOrganization())) {
-            employeeEntity.organization = employee.getOrganization();
-        }
-        if (Objects.nonNull(employee.getCode())) {
-            employeeEntity.code = employee.getCode();
-        }
-        if (Objects.nonNull(employee.getName())) {
-            employeeEntity.name = employee.getName();
-        }
-        if (Objects.nonNull(employee.getShortName())) {
-            employeeEntity.shortName = employee.getShortName();
-        }
-        if (Objects.nonNull(employee.getDesignation())) {
-            employeeEntity.designation = employee.getDesignation();
-        }
-        if (Objects.nonNull(employee.getNotes())) {
-            employeeEntity.notes = employee.getNotes();
-        }
-        if (Objects.nonNull(employee.getStatus())) {
-            employeeEntity.status = employee.getStatus();
-        }
-        if (Objects.nonNull(employee.getCreatedBy())) {
-            employeeEntity.createdBy = employee.getCreatedBy();
-        }
-        if (Objects.nonNull(employee.getUpdatedBy())) {
-            employeeEntity.updatedBy = employee.getUpdatedBy();
-        }
-        if (Objects.nonNull(employee.getCreatedAt())) {
-            employeeEntity.createdAt = employee.getCreatedAt();
-        }
-        if (Objects.nonNull(employee.getUpdatedAt())) {
-            employeeEntity.updatedAt = employee.getUpdatedAt();
-        }
-        return Response.noContent().build();
+        return Response.ok(employeeEntity).build();
     }
 
     // Delete employee by ID
